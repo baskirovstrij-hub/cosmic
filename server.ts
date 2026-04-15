2f3fe753425206b5483a955a4806c8e89a389643
baskirovstrij-hub <baskirovstrij@gmail.com>
2026-04-15 16:22:35 +0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -6,6 +6,8 @@ import { calculateNatalData } from "./src/services/astrologyService.js";
 import { mapNatalDataToInterpretations } from "./src/services/interpretationService.js";
 import { getDailyHoroscope } from "./src/services/horoscopeService.js";
 
+import fs from "fs";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -262,7 +264,12 @@ async function startServer() {
     const distPath = path.join(process.cwd(), 'dist');
     app.use(express.static(distPath));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
+      const indexPath = path.join(distPath, 'index.html');
+      if (fs.existsSync(indexPath)) {
+        res.sendFile(indexPath);
+      } else {
+        res.status(500).send('Build folder (dist) not found. Please ensure "vite build" runs during deployment.');
+      }
     });
   }
 
